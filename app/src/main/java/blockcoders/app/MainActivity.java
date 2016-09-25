3f474301aced01ec20214818543769595599263a
rugwing <aac.wong@gmail.com>
2016-09-24 23:03:27 -0500

    profile
    almost done with profile
    
    added image capability and started from MainActivity for debug

--- app/src/main/java/blockcoders/app/MainActivity.java
+++ app/src/main/java/blockcoders/app/MainActivity.java
@@ -1,6 +1,11 @@
 package blockcoders.app;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        goToProfileActivity();
 //        setContentView(R.layout.activity_main);
     }
+    public void goToProfileActivity() {
+        Intent intent = new Intent(this, profile.class);
+        startActivity(intent);
+    }
+
 }
+
--- app/src/main/java/blockcoders/app/profile.java
+++ app/src/main/java/blockcoders/app/profile.java
@@ -1,7 +1,11 @@
 package blockcoders.app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class profile extends AppCompatActivity {
 
@@ -9,6 +13,48 @@ public class profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Bitmap bm = imagePlace(R.drawable.totoro, 150, 150);
+        ImageView image = (ImageView) findViewById(R.id.imageView);
+        image.setImageBitmap(bm);
+        image.setImageResource(R.drawable.totoro);
+    }
+    private Bitmap imagePlace(int resId, int reqWidth, int reqHeight)
+    {
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(getResources(), resId, options);
+
+    }
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 }
 
